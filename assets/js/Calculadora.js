7685be222dbc758a9a789c1da0c426dc9a9677da
Astor Dioguardi <astor.dioguardi@hotmail.com>
2023-10-19 18:39:45 -0300

    realizo prueba de commit

---
+++ assets/js/Calculadora.js
@@ -0,0 +1,17 @@
+class Calculadora {
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2){
+        return num1 * num2;
+    }
+
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+}
